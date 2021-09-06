c2f8dcb966c52c541e9c9902b31c4e1c3aabb444
soselskyi <serhii.oselskyi@eleks.com>
2021-09-06 14:07:48 +0300

    new

---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div
+      v-for="todo in todos"
+      :key="todo.id"
+    >
+      {{ todo.label }}
+      <button @click="onDelete(todo.id)">Delete {{ todo.id }}</button>
+      <todo-item @onDelete="onDelete" /> 
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoPage',
+  props: ['todos'],
+
+  methods: {
+    onDelete (id) {
+      this.$emit('onDelete', id)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
